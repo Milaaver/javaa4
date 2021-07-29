ed915f23d678874306f5b7cd0205f94e03d9f4df
Milaaver <84475809+Milaaver@users.noreply.github.com>
2021-07-29 13:36:26 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,19 @@ public class Main {
         int intialAccount = 100;
         int depositAmount = 100;
         int finalAccount = intialAccount + depositAmount;
-        if (depositAmount < 1000) {
-            System.out.println("Final Account");
-            System.out.println(finalAccount);
-            System.out.println("No_Bonus");
-        } else {
-           int bonus = (intialAccount + depositAmount) / 100;
+        if (depositAmount > 1000) {
+            int bonus = (intialAccount + depositAmount) / 100;
+            int finalSum = finalAccount + bonus;
+            System.out.println("Final Sum");
+            System.out.println(finalSum) ;
             System.out.println("Bonus");
             System.out.println(bonus) ;
+        } else {
+
+            System.out.println("No_Bonus");
             System.out.println("Final Account");
             System.out.println(finalAccount);
         }
 
     }
 }
-
