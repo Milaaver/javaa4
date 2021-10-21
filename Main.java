aacdb9158f471f2d43201a571bb830da71a1ebe6
Milaaver <84475809+Milaaver@users.noreply.github.com>
2021-10-21 20:12:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         int intialAccount = 100;
-        int depositAmount = 900;
-      if (depositAmount > 1000) {
-            int bonus = (intialAccount + depositAmount) / 100;
+        int depositAmount = 1200;
+        int bonus;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
             int finalSum = intialAccount + depositAmount + bonus;
             System.out.println("Final Sum");
             System.out.println(finalSum) ;
             System.out.println("Bonus");
             System.out.println(bonus) ;
         } else {
-
+            bonus = 0;
             System.out.println("No_Bonus");
             System.out.println("Final Sum");
-            int bonus =0;
-            int finalSum = intialAccount + depositAmount;
-            System.out.println(finalSum);
         }
+
+
 
     }
 }
